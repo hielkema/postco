e820d323346ee612fcc0c1dde9ed60e638de18ba
Sander Mertens <49483781+mertenssander@users.noreply.github.com>
2020-10-26 16:05:24 +0100

    Update met een iets logischere template als test

--- vue/src/store/modules/templateBackend.js
+++ vue/src/store/modules/templateBackend.js
@@ -5,7 +5,49 @@ const state = {
     loading: false,
 
     // Example template #2
-    requestedTemplate: {
+    requestedTemplate: 
+    // {
+    //   'id' : 2,            // Unique, primary key
+    //   'time' : '1602058089',
+    //   'authors' : [
+    //     {
+    //       'name' : 'Test Test',
+    //       'contact' : 'test@test.nl',
+    //     }
+    //   ],
+    //   'title' : 'Test Template 2',                // Titel die in de frontend getoond wordt
+    //   'description' : 'Bedoeld om te testen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis lorem id mi venenatis, sed commodo neque tristique. Aliquam eget turpis placerat, aliquet dolor sed, vehicula est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet erat semper, hendrerit odio a, molestie lacus. Sed rhoncus sapien leo, nec luctus dolor pretium a. Integer mauris dui, viverra placerat vehicula id, egestas vel elit. Aenean posuere enim eget diam cursus, vel egestas tellus scelerisque. Quisque cursus porta dictum. Donec vel metus libero. Phasellus sollicitudin vel felis in venenatis.',    // Beschrijving die in de frontend getoond wordt
+    //   'snomedVersion' : '2020-09-30',               // SNOMED-versie waarop de template gebaseerd is
+    //   'snomedBranch' : 'MAIN/SNOMEDCT-NL',        // SNOMED-branch in Snowstorm (MAIN/SNOMEDCT-NL of SNOMEDCT-NL voor NL editie)
+    //   'template' : {
+    //     'focus' : ['74400008','272379006'],  // Focusconcepten voor template
+    //     'groups' : [       // Attribuutgroepen
+    //       [              // Groep 1
+    //         {
+    //           'title' : 'Veroorzaakt door', 
+    //           'description' : 'Hiermee kan je aangeven wat de veroorzaker is van de aandoening',    // Beschrijving: hoe moet het vak gebruikt worden?
+    //           'attribute' : '246075003',                                                          // SNOMED ID van het attribuut
+    //           'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
+    //         },
+    //         {
+    //           'title' : 'Substantie', 
+    //           'description' : 'Wat voor substantie?',    // Beschrijving: hoe moet het vak gebruikt worden?
+    //           'attribute' : '105590001',                                                          // SNOMED ID van het attribuut
+    //           'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
+    //         },
+    //       ],
+    //       [              // Groep 2
+    //         {
+    //           'title' : 'Veroorzaakt door', 
+    //           'description' : 'Hiermee kan je aangeven wat de veroorzaker is van de aandoening',    // Beschrijving: hoe moet het vak gebruikt worden?
+    //           'attribute' : '246075003',                                                          // SNOMED ID van het attribuut
+    //           'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
+    //         }
+    //       ]
+    //     ]
+    //   }
+    // },
+    {
       'id' : 2,            // Unique, primary key
       'time' : '1602058089',
       'authors' : [
@@ -13,35 +55,27 @@ const state = {
           'name' : 'Test Test',
           'contact' : 'test@test.nl',
         }
-      ],        
-      'title' : 'Test Template 2',                // Titel die in de frontend getoond wordt
-      'description' : 'Bedoeld om te testen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis lorem id mi venenatis, sed commodo neque tristique. Aliquam eget turpis placerat, aliquet dolor sed, vehicula est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet erat semper, hendrerit odio a, molestie lacus. Sed rhoncus sapien leo, nec luctus dolor pretium a. Integer mauris dui, viverra placerat vehicula id, egestas vel elit. Aenean posuere enim eget diam cursus, vel egestas tellus scelerisque. Quisque cursus porta dictum. Donec vel metus libero. Phasellus sollicitudin vel felis in venenatis.',    // Beschrijving die in de frontend getoond wordt
+      ],
+      'title' : 'Beeldvormende verrichting',                // Titel die in de frontend getoond wordt
+      'description' : 'Bedoeld om te testen. Usecase: Coderen van beeldvormende verrichtingen op een lichaamsstructuur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis lorem id mi venenatis, sed commodo neque tristique. Aliquam eget turpis placerat, aliquet dolor sed, vehicula est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet erat semper, hendrerit odio a, molestie lacus. Sed rhoncus sapien leo, nec luctus dolor pretium a. Integer mauris dui, viverra placerat vehicula id, egestas vel elit. Aenean posuere enim eget diam cursus, vel egestas tellus scelerisque. Quisque cursus porta dictum. Donec vel metus libero. Phasellus sollicitudin vel felis in venenatis.',    // Beschrijving die in de frontend getoond wordt
       'snomedVersion' : '2020-09-30',               // SNOMED-versie waarop de template gebaseerd is
       'snomedBranch' : 'MAIN/SNOMEDCT-NL',        // SNOMED-branch in Snowstorm (MAIN/SNOMEDCT-NL of SNOMEDCT-NL voor NL editie)
       'template' : {
-        'focus' : ['74400008','272379006'],  // Focusconcepten voor template
+        'focus' : ['71388002'],  // Focusconcepten voor template
         'groups' : [       // Attribuutgroepen
           [              // Groep 1
             {
-              'title' : 'Veroorzaakt door', 
-              'description' : 'Hiermee kan je aangeven wat de veroorzaker is van de aandoening',    // Beschrijving: hoe moet het vak gebruikt worden?
-              'attribute' : '246075003',                                                          // SNOMED ID van het attribuut
-              'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
+              'title' : 'Methode', 
+              'description' : 'Welke methode wordt er gebruikt?',    // Beschrijving: hoe moet het vak gebruikt worden?
+              'attribute' : '260686004 ',                                                          // SNOMED ID van het attribuut
+              'value' : '< 360037004 |beeldvorming (kwalificatiewaarde)|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
             },
             {
-              'title' : 'Substantie', 
-              'description' : 'Wat voor substantie?',    // Beschrijving: hoe moet het vak gebruikt worden?
-              'attribute' : '105590001',                                                          // SNOMED ID van het attribuut
-              'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
+              'title' : 'Procedure site', 
+              'description' : 'Welke locatie?',    // Beschrijving: hoe moet het vak gebruikt worden?
+              'attribute' : '405813007',                                                          // SNOMED ID van het attribuut
+              'value' : '< 442083009 |Anatomical or acquired body structure (body structure)|  ',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
             },
-          ],
-          [              // Groep 2
-            {
-              'title' : 'Veroorzaakt door', 
-              'description' : 'Hiermee kan je aangeven wat de veroorzaker is van de aandoening',    // Beschrijving: hoe moet het vak gebruikt worden?
-              'attribute' : '246075003',                                                          // SNOMED ID van het attribuut
-              'value' : '<< 105590001 |Substance (substance)| OR 138875005 |SNOMED CT Concept (SNOMED RT+CTV3)| OR << 260787004 |Physical object (physical object)| OR << 373873005 |Pharmaceutical / biologic product (product)| OR << 410607006 |Organism (organism)| OR << 78621006 |Physical force (physical force)| OR <<404684003 |clinical finding|',                                         // ECL query met valide attribute values (dit is niet per se een valide voorbeeld)
-            }
           ]
         ]
       }
