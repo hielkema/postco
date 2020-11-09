e98bcd32d15eae7fbf1ec99d1084d1e57440540a
Bas Harmsen <b.harmsen@iknl.nl>
2020-11-09 16:58:13 +0100

    TemplateService: Add list operation

--- SnomedTemplateService/SnomedTemplateService.Data/XmlFileTemplateRepository.cs
+++ SnomedTemplateService/SnomedTemplateService.Data/XmlFileTemplateRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security;
 using System.Xml;
 using Microsoft.Extensions.FileProviders;
@@ -29,35 +30,62 @@ namespace SnomedTemplateService.Data
             {
                 return null;
             }
-            var result = new TemplateData(
-                id,
-                DateTime.Parse(templateNode.SelectSingleNode("time")?.InnerText?.Trim(), CultureInfo.InvariantCulture),
-                templateNode.SelectSingleNode("snomedVersion")?.InnerText?.Trim(),
-                templateNode.SelectSingleNode("snomedBranch")?.InnerText?.Trim(),
-                templateNode.SelectSingleNode("etl")?.InnerText?.Trim()
-            )
+            else
             {
-                Description = templateNode.SelectSingleNode("description")?.InnerText?.Trim(),
-            };
-            var title = templateNode.SelectSingleNode("title")?.InnerText?.Trim();
-            if (title!= null)
-            {
-                result.Title = title;
+                return ExtractTemplateDataFromNode(templateNode);
             }
-            result.StringFormat = templateNode.SelectSingleNode("stringFormat")?.InnerText?.Trim();
-            result.SlotTitles = templateNode.SelectNodes("slots/slot").Cast<XmlElement>().ToDictionary(e => e.GetAttribute("name"), e => e.SelectSingleNode("title")?.InnerText ?? e.GetAttribute("name"));
-            result.SlotTitles = result.SlotTitles.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
-            result.SlotDescriptions = templateNode.SelectNodes("slots/slot").Cast<XmlElement>().ToDictionary(e => e.GetAttribute("name"), e => e.SelectSingleNode("description")?.InnerText);
-            result.SlotDescriptions = result.SlotDescriptions.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
-            result.Authors = templateNode.SelectNodes("authors/author")
-                .Cast<XmlElement>()
-                .Select(a=>new TemplateAuthor(a.SelectSingleNode("name")?.InnerText?.Trim()) 
-                {
-                    Contact = a.SelectSingleNode("contact")?.InnerText?.Trim()
-                }).ToList();
-
-            return result;
              
         }
+
+        public IList<TemplateData> GetTemplates()
+        {
+            return sourceDoc.SelectNodes($"//template")
+                .Cast<XmlNode>()
+                .Select(ExtractTemplateDataFromNode)
+                .ToList();
+        }
+
+        private TemplateData ExtractTemplateDataFromNode(XmlNode templateNode)
+        {
+            if (int.TryParse(templateNode.Attributes["id"]?.Value, out int id))
+            {
+                var result = new TemplateData(
+                    id,
+                    DateTime.Parse(templateNode.SelectSingleNode("time")?.InnerText?.Trim(), CultureInfo.InvariantCulture),
+                    templateNode.SelectSingleNode("snomedVersion")?.InnerText?.Trim(),
+                    templateNode.SelectSingleNode("snomedBranch")?.InnerText?.Trim(),
+                    templateNode.SelectSingleNode("etl")?.InnerText?.Trim()
+                )
+                {
+                    Description = templateNode.SelectSingleNode("description")?.InnerText?.Trim(),
+                };
+
+                var title = templateNode.SelectSingleNode("title")?.InnerText?.Trim();
+                if (title != null)
+                {
+                    result.Title = title;
+                }
+                result.StringFormat = templateNode.SelectSingleNode("stringFormat")?.InnerText?.Trim();
+                result.SlotTitles = templateNode.SelectNodes("slots/slot").Cast<XmlElement>().ToDictionary(e => e.GetAttribute("name"), e => e.SelectSingleNode("title")?.InnerText ?? e.GetAttribute("name"));
+                result.SlotTitles = result.SlotTitles.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+                result.SlotDescriptions = templateNode.SelectNodes("slots/slot").Cast<XmlElement>().ToDictionary(e => e.GetAttribute("name"), e => e.SelectSingleNode("description")?.InnerText);
+                result.SlotDescriptions = result.SlotDescriptions.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+                result.Authors = templateNode.SelectNodes("authors/author")
+                    .Cast<XmlElement>()
+                    .Select(a => new TemplateAuthor(a.SelectSingleNode("name")?.InnerText?.Trim())
+                    {
+                        Contact = a.SelectSingleNode("contact")?.InnerText?.Trim()
+                    }).ToList();
+
+                return result;
+            }
+            else
+            {
+                // TODO logging
+                return null;
+            }
+
+        }
+        
     }    
 }
