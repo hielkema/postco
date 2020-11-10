54893df3b1314ae4adc62dd4eef3269c9590c173
root <user.name>
2020-11-10 15:27:19 +0100

    Add debug variable to store

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -10,13 +10,16 @@ Vue.use(axios)
 
 var envBaseUrl = 'https://postco.test-nictiz.nl/'
 var envBaseUrlTemplateservice = 'https://postco.test-nictiz.nl/'
+var debug = false
 if(process.env['NODE_ENV'] == 'development'){
   envBaseUrl = 'http://localhost:8000/',
   envBaseUrlTemplateservice = 'http://localhost:9125/'
+  debug = true
 }
 
 export default new Vuex.Store({
   state: {
+    debug: debug,
     baseUrl: envBaseUrl,
     baseUrlTemplateservice : envBaseUrlTemplateservice,
   },
